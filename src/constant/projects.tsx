f8c0966f3967baf989852aba05ffdb7666739219
sohail900 <sohailbalti000@gmail.com>
2024-03-24 00:37:37 +0500

    add new projects

--- src/constant/projects.tsx
+++ src/constant/projects.tsx
@@ -8,4 +8,13 @@ export const allProjects = [
             'Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries',
         tech: ['React.js', 'Tailwind Css', 'GPT Api'],
     },
+    {
+        id: 2,
+        github: 'https://github.com/sohail900/x_frontend',
+        externalLink: 'https://xtwitterms.vercel.app/',
+        title: 'X Twitter Clone',
+        description:
+            'Introducing X Twitter Clone, a dynamic and user-friendly social media platform inspired by the popular microblogging service. With X Twitter Clone, users can seamlessly engage with a familiar interface, posting tweets, following friends, and exploring trending topics.',
+        tech: ['React.js', 'Typescript', 'Scss', 'Redux', 'TanStack Query'],
+    },
 ]
