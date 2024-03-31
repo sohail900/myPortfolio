b98a56edd3b2cc509e25247d3a2e7d37ac56ca86
sohail900 <sohailbalti000@gmail.com>
2024-03-31 05:36:17 +0500

    add new features

--- src/components/project/children.tsx
+++ src/components/project/children.tsx
@@ -1,12 +1,16 @@
-import { FC } from 'react'
+import { useState } from 'react'
 import { FiGithub, FiExternalLink } from 'react-icons/fi'
 import { CiFolderOn } from 'react-icons/ci'
 import { IProject } from '../../types/type'
 const Children = (props: IProject) => {
+    const [readMore, setReadMore] = useState(false)
     const { id, description, externalLink, github, title, tech } = props.values // Destructuring props
     const project_desp: string = description
     const slice_project_desp = project_desp.slice(0, 90)
-
+    //read more handler.
+    const readmoreHandler = () => {
+        setReadMore((pre) => !pre)
+    }
     return (
         <>
             <div className='project_icons'>
@@ -23,7 +27,12 @@ const Children = (props: IProject) => {
                 </div>
             </div>
             <h2 className='project_title'>{title}</h2>
-            <p className='project_desp'>{slice_project_desp}...</p>
+            <p className='project_desp'>
+                {readMore ? description : slice_project_desp + '...'}
+                <button className='btn' onClick={readmoreHandler}>
+                    {readMore ? 'read less' : 'read more'}
+                </button>
+            </p>
             <div className='technlogies'>
                 {tech.map((tech, index) => (
                     <code key={index}>{tech}</code>
